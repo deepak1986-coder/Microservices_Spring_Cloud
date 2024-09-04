4527365602d5c745731ca796c264273d2ec1a993
DELL <deshdeepak.d3@gmail.com>
2024-09-04 19:55:32 +0530

    Added limit service and and reading property file from git repository using spring cloud.

--- src/main/java/com/dev/microservices/limit_service/controller/LimitController.java
+++ src/main/java/com/dev/microservices/limit_service/controller/LimitController.java
@@ -1,17 +1,23 @@
 package com.dev.microservices.limit_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.microservices.limit_service.beans.Limits;
+import com.dev.microservices.limit_service.configuration.Configuration;
 
 @RestController
 public class LimitController {
 
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
 
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+		// return new Limits(1, 1000);
 	}
 
 }
